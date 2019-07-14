09f44f9aafe5efe83ed6b5788b7e9b5ae1ba7adf
Carl Argabright <carl.argabright@gmail.com>
2019-07-14 14:40:54 -0700

    Initial Commit

---
+++ Models/Repository.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace PartyInvites.Models {
+
+    public class Repository {
+
+        private static List<GuestResponse> responses = new List<GuestResponse>();
+        public static IEnumerable<GuestResponse> Responses {
+            get { return Responses; }
+        }
+
+        public static void AddResponse(GuestResponse response) {
+            responses.Add(response);
+        }
+    }
+
+}
