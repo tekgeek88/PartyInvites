6844ae7a7dd77f4b12395bb5782758a9185a9948
Carl Argabright <carl.argabright@gmail.com>
2019-07-14 22:27:47 -0700

    Fixed bug caused by requesting list of attendees and added bootstrap styling

--- Models/Repository.cs
+++ Models/Repository.cs
@@ -6,7 +6,7 @@ namespace PartyInvites.Models {
 
         private static List<GuestResponse> responses = new List<GuestResponse>();
         public static IEnumerable<GuestResponse> Responses {
-            get { return Responses; }
+            get { return responses; }
         }
 
         public static void AddResponse(GuestResponse response) {
